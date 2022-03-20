7d98b4c70ec0b598ea66cc623c43568b07aec537
KorolkovOleg <helkorolkov@gmail.com>
2022-03-21 01:53:48 +0300

    Initial commit

---
+++ src/main/java/com/example/projectlaminam/ProjectLaminamApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectlaminam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLaminamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLaminamApplication.class, args);
+    }
+
+}
