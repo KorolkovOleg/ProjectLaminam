d01d00b463d78317a85c7c0efb9c002f82381754
KorolkovOleg <helkorolkov@gmail.com>
2022-03-26 01:42:40 +0300

    Добавлена frontend реализация crud функционала для сущности Pack

---
+++ src/main/webapp/reactjs/src/PackEdit.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+import AppNavbar from './AppNavBar';
+
+class PackEdit extends Component {
+
+    emptyItem = {
+        name: ''
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: this.emptyItem
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async componentDidMount() {
+        if (this.props.match.params.id !== 'new') {
+            const pack = await (await fetch(`/packages/${this.props.match.params.id}`)).json();
+            this.setState({item: pack});
+        }
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        let item = {...this.state.item};
+        item[name] = value;
+        this.setState({item});
+    }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        const {item} = this.state;
+
+        await fetch('/packages' + (item.id ? '/' + item.id : ''), {
+            method: (item.id) ? 'PUT' : 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item),
+        });
+        this.props.history.push('/packages');
+    }
+
+    render() {
+        const {item} = this.state;
+        const title = <h2>{item.id ? 'Rename pack' : 'Create pack'}</h2>;
+
+        return <div>
+            <AppNavbar/>
+            <Container>
+                {title}
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="name">Name</Label>
+                        <Input type="text" name="name" id="name" value={item.name || ''}
+                               onChange={this.handleChange} autoComplete="name"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>{' '}
+                        <Button color="secondary" tag={Link} to="/packages">Cancel</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
+    }
+}
+export default withRouter(PackEdit);
---
+++ src/main/webapp/reactjs/src/PackList.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import AppNavbar from './AppNavBar';
+import { Link } from 'react-router-dom';
+
+class PackList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {packages: []};
+        this.remove = this.remove.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('/packages')
+            .then(response => response.json())
+            .then(data => this.setState({packages: data}));
+    }
+
+    async remove(id) {
+        await fetch(`/packages/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            let updatedPackages = [...this.state.packages].filter(i => i.id !== id);
+            this.setState({packages: updatedPackages});
+        });
+    }
+
+    render() {
+        const {packages, isLoading} = this.state;
+
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        const packagesList = packages.map(pack => {
+            return <tr key={pack.id}>
+                <td style={{whiteSpace: 'nowrap'}}>{pack.name}</td>
+                <td>
+                    <ButtonGroup>
+                        <Button size="sm" color="primary" tag={Link} to={"/packages/" + pack.id}>Edit</Button>
+                        <Button size="sm" color="danger" onClick={() => this.remove(pack.id)}>Delete</Button>
+                    </ButtonGroup>
+                </td>
+            </tr>
+        });
+
+        return (
+            <div>
+                <AppNavbar/>
+                <Container fluid>
+                    <div className="float-right">
+                        <Button color="success" tag={Link} to="/packages/new">Add Package</Button>
+                    </div>
+                    <h3>Packages</h3>
+                    <Table className="mt-4">
+                        <thead>
+                        <tr>
+                            <th width="30%">Name</th>
+                            <th width="40%">Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {packagesList}
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+    }
+}
+export default PackList;
