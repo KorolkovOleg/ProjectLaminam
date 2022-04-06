655e5bd879e009b1a2e9273f73295584c4b97957
KorolkovOleg <helkorolkov@gmail.com>
2022-04-06 21:58:01 +0300

    Реализована авторизация через JWT токен на стороне бэкенда

--- src/main/java/com/example/projectlaminam/config/SpringSecurityConfig.java
+++ src/main/java/com/example/projectlaminam/config/SpringSecurityConfig.java
@@ -1,17 +1,20 @@
 package com.example.projectlaminam.config;
 
+import com.example.projectlaminam.security.JwtConfigurer;
 import com.example.projectlaminam.security.Permission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,49 +24,35 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
+    private final JwtConfigurer jwtConfigurer;
 
-    @Autowired
-    public SpringSecurityConfig(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public SpringSecurityConfig(JwtConfigurer jwtConfigurer) {
+        this.jwtConfigurer = jwtConfigurer;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.GET, "/packages/").hasAuthority(Permission.CARDS_READ.getPermission())
-                .antMatchers(HttpMethod.GET, "/packages/**").hasAuthority(Permission.CARDS_READ.getPermission())
-                .antMatchers(HttpMethod.POST, "/packages/").hasAuthority(Permission.CARDS_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT, "/packages/**").hasAuthority(Permission.CARDS_WRITE.getPermission())
-                .antMatchers(HttpMethod.DELETE, "/packages/**").hasAuthority(Permission.CARDS_WRITE.getPermission())
-                .antMatchers(HttpMethod.GET, "/packages/**/cards").hasAuthority(Permission.CARDS_READ.getPermission())
-                .antMatchers(HttpMethod.POST, "/packages/**/cards").hasAuthority(Permission.CARDS_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT, "/packages/**/cards/**").hasAuthority(Permission.CARDS_WRITE.getPermission())
-                .antMatchers(HttpMethod.DELETE, "/packages/**/cards/**").hasAuthority(Permission.CARDS_WRITE.getPermission())
+                .antMatchers("/auth/login").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .apply(jwtConfigurer);
     }
 
+    @Bean
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
     protected PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
-    }
-
-    @Bean
-    protected DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        return daoAuthenticationProvider;
     }
 }
